6ed41165b2efd70583f26cae57866ad0971d96cc
audrey352 <aberniercan@gmail.com>
2024-10-18 01:21:32 -0400

    updated q3

--- pset5/question3.py
+++ pset5/question3.py
@@ -5,23 +5,6 @@ hbar = const.hbar
 np.set_printoptions(precision=4) #settings to make arrays easier to read
 np.set_printoptions(suppress=True)
 
-# Operators for spin-1
-Jz = np.diag([1,0,-1])
-Jx = np.array([[0,1,0],[1,0,1],[0,1,0]])/np.sqrt(2)
-Jy = np.array([[0,-1j,0],[1j,0,-1j],[0,1j,0]])/np.sqrt(2)
-
-# Jx eigenstates
-ex,vx = np.linalg.eigh(Jx)
-ex,vx = np.flipud(ex), np.fliplr(vx)
-xbra=np.conj(vx).T  # z to x
-# xket=vx  # x to z
-
-# Jy eigenstates
-ey,vy = np.linalg.eigh(Jy)
-ey,vy = np.flipud(ey), np.fliplr(vy)
-ybra=np.conj(vy).T  # z to y
-# yket=vy  # y to z
-
 # Functions
 def compute_prob(SGn, state, nbra, en):
     """
@@ -52,6 +35,22 @@ def R_from_J(J,th):
     e,v=np.linalg.eigh(J)
     e_new=-1j*th*e
     return v@np.diag(np.exp(e_new))@v.conj().T
+
+
+# Operators for spin-1
+Jz = np.diag([1,0,-1])
+Jx = np.array([[0,1,0],[1,0,1],[0,1,0]])/np.sqrt(2)
+Jy = np.array([[0,-1j,0],[1j,0,-1j],[0,1j,0]])/np.sqrt(2)
+
+# Jx eigenstates
+ex,vx = np.linalg.eigh(Jx)
+ex,vx = np.flipud(ex), np.fliplr(vx)
+xbra=np.conj(vx).T  # z to x
+
+# Jy eigenstates
+ey,vy = np.linalg.eigh(Jy)
+ey,vy = np.flipud(ey), np.fliplr(vy)
+ybra=np.conj(vy).T  # z to y
 
 
 # Part A -------------------------------------
